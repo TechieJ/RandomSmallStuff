9fb812c8c09f20d0cf3f496ac83bafb47b8fdc22
Jaideep Singh <er.jaideepsahni@gmail.com>
2025-06-16 21:59:57 +0530

    Update list_flattening.py

--- list_flattening.py
+++ list_flattening.py
@@ -15,12 +15,34 @@ def flatten(seq):
 a = [[1, 2, 3], [4]]
 b = list(flatten(a))
 
-# Method 2: Library solution using itertools.chain.from_iterable ( Faster than any method for large inputs as its written in C )
+# Method 2: Library solution using itertools.chain.from_iterable ( Faster than any method for large inputs having single level nesting as its written in C )
 from itertools import chain
 b = list(chain.from_iterables(a))
 
-# Method 3: Comprehension (Clean and Fast but you should know the level of nesting)
+# Method 3: Comprehension (Clean and Fast but good for small inputs and single level nesting)
 b = [item for sublist in a for item in sublist]
 
 # Method 4: Star unpacking (When you already know the length)
 b = [*a[0], *a[1]]
+
+# Method 5:  Iterative stack (No recurrsion)
+from collections import deque
+from collections.abc import Iterable
+
+_ATOMIC = (str, bytes, bytearray) # Just to ignore these as iterables in if condition
+
+def deep_flatten(seq):
+  stack = deque([iter(seq)]) # start with top iterator
+  while stack:
+    try:
+      item = next(stack[-1]) # look at the current iterator
+    except StopIteration:  
+      stack.pop()            # Exhausted pop it
+      continue
+
+    if isinstance(item, Iterable) and not isinstance(item, _ATOMIC):
+      stack.append(iter(item))
+    else:
+      yield item
+
+b = list(deep_flatten(a))
